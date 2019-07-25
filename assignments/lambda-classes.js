2cb84a32e40e6e79bb586d8a40a60da7521a93c3
Adrian-Garcia <agarc5@hotmail.com>
2019-07-25 15:59:29 -0500

    class for person and instructor complete + student

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,46 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor (attributes) {
+      this.name = attributes.name;
+      this.age = attributes.age;
+      this.location = attributes.location;
+      this.gender = attributes.gender;
+    }
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
+    }
+}
+
+class Instructor extends Person{
+    constructor (instructorAttributes){
+        super(instructorAttributes);
+        this.specialty = instructorAttributes.specialty;
+        this.favLanguage = instructorAttributes.favLanguage;
+        this.catchPhrase = instructorAttributes.catchPhrase;
+    }
+    demo(subject){
+        return `Today we are learning about ${subject}.`;
+    }
+    grade(student, subject){
+        return `${student}receives a perfect score on ${subject}`;
+    }
+
+}
+
+class Student extends Instructor{
+    constructor (studentAttributes){
+        super(studentAttributes);
+        this.previousBackground = studentAttributes.previousBackground;
+        this.className = studentAttributes.className;
+        this.favSubjects = studentAttributes.favSubjects;
+    }
+    listsSubjects(){
+
+    }
+    PRAssignment(){
+
+    }
+    sprintChallenge(){
+        
+    }
+}
